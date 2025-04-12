cb027f91469246e8fe5b1e8c486aa128e32e85a3
sunniesfish <hhch5611.jh@gmail.com>
2025-04-12 14:35:48 +0900

    custom hook : useDeviceType

---
+++ src/lib/hook.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+export enum DeviceType {
+  Mobile = "mobile",
+  Tablet = "tablet",
+  Desktop = "desktop",
+}
+
+type DeviceTypeOption =
+  | DeviceType.Desktop
+  //   | DeviceType.Tablet
+  | DeviceType.Mobile;
+
+export const useDeviceType = (deviceTypeOption: DeviceTypeOption) => {
+  const query = (() => {
+    switch (deviceTypeOption) {
+      case DeviceType.Desktop:
+        return "(min-width: 768px)";
+      //   case DeviceType.Tablet:
+      //     return "(min-width: 640px) and (max-width: 1023px)";
+      case DeviceType.Mobile:
+        return "(max-width: 767px)";
+      default:
+        return "(max-width: 767px)";
+    }
+  })();
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(query);
+    const handleChange = (event: MediaQueryListEvent) => {
+      setMatches(event.matches);
+    };
+    mediaQueryList.addEventListener("change", handleChange);
+    setMatches(mediaQueryList.matches);
+    return () => {
+      mediaQueryList.removeEventListener("change", handleChange);
+    };
+  }, [query]);
+  return matches;
+};
