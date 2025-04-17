dc910f1d6e6627ac4cb7dc0756576924944addf7
sunniesfish <hhch5611.jh@gmail.com>
2025-04-18 01:35:00 +0900

    hotfix : remove top-level await

--- src/lib/idb.ts
+++ src/lib/idb.ts
@@ -1,19 +1,30 @@
 import { DB_NAME, STORE_NAME, MusicDb, Music } from "@/type/music-type";
-import { openDB } from "idb";
+import { openDB, IDBPDatabase } from "idb";
 
-const musicDB = await openDB<MusicDb>(DB_NAME, 1, {
-  upgrade(db) {
-    const store = db.createObjectStore(STORE_NAME, { keyPath: "id" });
-    store.createIndex("by_date", "upload_date");
-  },
-});
+let musicDB: IDBPDatabase<MusicDb> | null = null;
+
+async function initDB() {
+  if (!musicDB) {
+    musicDB = await openDB<MusicDb>(DB_NAME, 1, {
+      upgrade(db) {
+        const store = db.createObjectStore(STORE_NAME, { keyPath: "id" });
+        store.createIndex("by_date", "upload_date");
+      },
+    });
+  }
+  return musicDB;
+}
+
+void initDB();
 
 export async function addMusic(music: Music) {
+  if (!musicDB) await initDB();
+
   const size = music.file.size;
   if (size > 10 * 1024 * 1024) {
     throw new Error("File size is too large");
   }
-  const tx = musicDB.transaction(STORE_NAME, "readwrite");
+  const tx = musicDB!.transaction(STORE_NAME, "readwrite");
   try {
     await tx.store.add(music);
     await tx.done;
@@ -25,7 +36,9 @@ export async function addMusic(music: Music) {
 }
 
 export async function getMusicList() {
-  const tx = musicDB.transaction(STORE_NAME, "readonly");
+  if (!musicDB) await initDB();
+
+  const tx = musicDB!.transaction(STORE_NAME, "readonly");
   try {
     const list = await tx.store.index("by_date").getAll();
     await tx.done;
@@ -37,7 +50,9 @@ export async function getMusicList() {
 }
 
 export async function getMusic(id: string) {
-  const tx = musicDB.transaction(STORE_NAME, "readonly");
+  if (!musicDB) await initDB();
+
+  const tx = musicDB!.transaction(STORE_NAME, "readonly");
   try {
     const music = await tx.store.get(id);
     await tx.done;
@@ -49,7 +64,9 @@ export async function getMusic(id: string) {
 }
 
 export async function deleteMusic(id: string) {
-  const tx = musicDB.transaction(STORE_NAME, "readwrite");
+  if (!musicDB) await initDB();
+
+  const tx = musicDB!.transaction(STORE_NAME, "readwrite");
   try {
     await tx.store.delete(id);
     await tx.done;
